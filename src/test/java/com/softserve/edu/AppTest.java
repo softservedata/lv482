2386b8fe1fc3efac62e38e9d076a77ec7be0b83b
Yaroslav Harasym <yharasym@training.local>
2019-12-24 16:56:45 +0200

    Allure. Example

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,38 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("\t\tclass AppTest: @BeforeClass");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		System.out.println("\t\tclass AppTest: @AfterClass");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("\t\t\tclass AppTest: @BeforeMethod");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("\t\t\tclass AppTest: @AfterMethod");
+	}
+
+	@Test
+	public void testApp() {
+		System.out.println("\t\t\t\tclass AppTest: @Test");
+		Assert.assertTrue(true);
+	}
+	
+}
