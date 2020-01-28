9fc4da7cda99e62fbd6f47b5ca5e1d563849b962
rosomaha209 <rosomaha209@gmail.com>
2020-01-28 18:39:51 +0200

    HomeWork1

--- src/com/softserve/edu/lv482/HomeWork.java
+++ src/com/softserve/edu/lv482/HomeWork.java
@@ -3,15 +3,34 @@ package com.softserve.edu.lv482;
 import java.util.Scanner;
 public class HomeWork {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Введіть Радіус:");
         double radius;
-        Scanner sc =new Scanner(System.in);
         radius = sc.nextDouble();
         double area;
         area = Math.PI *(radius * radius);
         System.out.println("Площа кола дорівнює: " + area);
        double circumference;
        circumference = Math.PI * 2 * radius;
-        System.out.println("Периметр дорівнює: " +circumference );
+        System.out.println("Периметр дорівнює: " + circumference);
+        //Щось тут не так
+
+        System.out.print("Ваше Ім'я:");
+        String name = sc.nextLine();
+        System.out.print("Ваш Адрес: ");
+        String address = sc.nextLine();
+        System.out.println("Ім*я " + name + " за адресою: " + address);
+
+        System.out.print("С1 ");
+        double C1 = sc.nextDouble();
+        System.out.print("C2 ");
+        double C2 = sc.nextDouble();
+        System.out.print("C3 ");
+        double C3 = sc.nextDouble();
+        System.out.print("Ціна за хв: ");
+        double prise = sc.nextDouble();
+        System.out.println("Загальна Тривалість: " + (C1 + C2 + C3));
+        System.out.println("Загальна Вартість: " + (C1 + C2 + C3 * prise));
+        sc.close();
     }
 }
