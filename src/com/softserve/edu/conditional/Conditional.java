0176c17e99e64d9ba6cd6d24b18bb1d55134e2e7
rosomaha209 <rosomaha209@gmail.com>
2020-02-04 18:21:44 +0200

    conditionalTask3Roman

--- src/com/softserve/edu/conditional/Conditional.java
+++ src/com/softserve/edu/conditional/Conditional.java
@@ -1,29 +1,29 @@
-package com.softserve.edu.conditional;
-
-import java.util.Scanner;
-
-public class Conditional {
-   public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        float a = scanner.nextFloat();
-        System.out.print("Enter second number: ");
-        float b = scanner.nextFloat();
-        System.out.print("Enter third number: ");
-        float c = scanner.nextFloat();
-        //float d = a * b * c ;
-        //System.out.print(d);
-        if (-5 < a && a < 5){System.out.println("Its OK first =  " + a);}
-        else if (a >= 5){System.out.println("error first number, enter number not more than 5");}
-        else if (a <= -5){System.out.println("error first number, enter number not loss than -5");}
-        if (-5 < b && b < 5){System.out.println("Its OK second =  " + b);}
-        else if (b >= 5){System.out.println("error second number, enter number not more than 5");}
-        else if (b <= -5){System.out.println("error second number, enter number not loss than -5");}
-        if (-5 < c && c < 5){System.out.println("Its OK third =  " + c);}
-        else if (c >= 5){System.out.println("error third number, enter number not more than 5");}
-        else if (c <= -5){System.out.println("error third number, enter number not loss than -5");}
-
-
-
-    }
-}
+//package com.softserve.edu.conditional;
+//
+//import java.util.Scanner;
+//
+//public class Conditional {
+//   public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter first number: ");
+//        float a = scanner.nextFloat();
+//        System.out.print("Enter second number: ");
+//        float b = scanner.nextFloat();
+//        System.out.print("Enter third number: ");
+//        float c = scanner.nextFloat();
+//        //float d = a * b * c ;
+//        //System.out.print(d);
+//        if (-5 < a && a < 5){System.out.println("Its OK first =  " + a);}
+//        else if (a >= 5){System.out.println("error first number, enter number not more than 5");}
+//        else if (a <= -5){System.out.println("error first number, enter number not loss than -5");}
+//        if (-5 < b && b < 5){System.out.println("Its OK second =  " + b);}
+//        else if (b >= 5){System.out.println("error second number, enter number not more than 5");}
+//        else if (b <= -5){System.out.println("error second number, enter number not loss than -5");}
+//        if (-5 < c && c < 5){System.out.println("Its OK third =  " + c);}
+//        else if (c >= 5){System.out.println("error third number, enter number not more than 5");}
+//        else if (c <= -5){System.out.println("error third number, enter number not loss than -5");}
+//
+//
+//
+//    }
+//}
