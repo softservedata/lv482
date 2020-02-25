b7b11f6073492418b7870bef162f7cf76ee2eba9
rosomaha209 <rosomaha209@gmail.com>
2020-02-25 20:44:27 +0200

    exeption

--- src/com/softserve/edu/exeption/ApplExeption.java
+++ src/com/softserve/edu/exeption/ApplExeption.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class ApplExeption {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Entered colors: RED, WHITE or BLUE: ");
-        String colors = scanner.nextLine();
-        System.out.println("Entered type: STUNTED or TALL: ");
-        String type = scanner.nextLine();
+        String colors = " ";
+        String type = " ";
+
         Plant[] flowers = new Plant[]{
                 new Plant(12, Color.RED, Type.STUNTED, "lilies"),
                 new Plant(6, Color.BLUE, Type.TALL, "mallow"),
@@ -16,32 +15,58 @@ public class ApplExeption {
                 new Plant(8, Color.WHITE, Type.STUNTED, "violets"),
                 new Plant(10, Color.BLUE, Type.STUNTED, "cornflowers")
         };
-        // for (i=0;i<flowers.length;i++){
-        System.out.println("Selected color: ");
         try {
-            for (Plant plant : flowers) {
-                if(plant.color == Color.valueOf(colors)) {
-                    System.out.println(plant);
+            System.out.println("What color or parameter to look for? if color entered 1, if type entered 2!");
+            int i = Integer.parseInt(scanner.nextLine());
+            if(i == 1) {
+                System.out.println("Entered colors: RED, WHITE or BLUE: ");
+                colors = scanner.nextLine();
+
+                System.out.println("Selected color: " + colors);
+                try {
+                    for (Plant plant : flowers) {
+                        if(plant.color == Color.valueOf(colors)) {
+                            System.out.println(plant);
+                        }
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Color entered incorrectly ");
+
+                } finally {
+                    System.out.println("Disigned by Roman");
                 }
+            } else if(i == 2) {
+                System.out.println("Entered type: STUNTED or TALL: ");
+                type = scanner.nextLine();
+                System.out.println("Selected type: " + type);
+                try {
+                    for (Plant plant2 : flowers) {
+                        if(plant2.type == Type.valueOf(type)) {
+                            System.out.println(plant2);
+                        }
+                    }
+
+                } catch (IllegalArgumentException e3) {
+                    System.out.println("Type entered incorrectly ");
+                } catch (Exception e2) {
+                    System.out.println("Mishap");
+                } finally {
+                    System.out.println("Disigned by Roman");
+                }
+            } else {
+                System.out.println("Iincorrectly type ");
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("Color entered incorrectly ");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Number Format Exception ");
+
+
+        } catch (Exception e2) {
+            System.out.println(" Format Exception ");
+
 
         }
-        System.out.println("Selected type: ");
-        try {
-            for (Plant plant2 : flowers) {
-                if(plant2.type == Type.valueOf(type))
-                {
-                    System.out.println(plant2);
-                }
-            }
 
-        } catch (IllegalArgumentException e) {
-            System.out.println("Type entered incorrectly ");
 
-        }finally {
-            System.out.println("Disigned by Roman");
-        }
     }
 }
